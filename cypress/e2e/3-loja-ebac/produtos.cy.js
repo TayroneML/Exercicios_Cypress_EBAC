b8ed3a2264377796b43595073960305e0ac639a3
TayroneML <tayrone@esolution.com.br>
2024-06-05 21:53:21 -0300

    Page Objects

--- cypress/e2e/3-loja-ebac/produtos.cy.js
+++ cypress/e2e/3-loja-ebac/produtos.cy.js
@@ -1,22 +1,29 @@
 /// <reference types="cypress" />
+import produtosPage from "../../support/page-objects/produtos.page"
 
 
 describe('Funcionalidade: Produtos', () => {
 
     beforeEach(() => {
-        cy.visit('/produtos/')
+        produtosPage.visitarUrl()
     })
 
-    it('Deve selecionar um produto da lista', () => {
-        cy.get('.products > .row')
-        // .first()
-        // .last()
-        // .eq(2)
-        .contains('Abominable Hoodie')
-        .click()  
-
+    it('Deve selecionar um produto na lista', () => {
+        produtosPage.buscarProdutoLista('Abominable Hoodie')
         cy.get('#tab-title-description > a').should('contain', 'Descrição')
-
     })
 
+    it('Deve busscar um produto com sucesso', () => {
+        let produto = 'Abominable Hoodie'
+        produtosPage.buscarProduto(produto)
+        cy.get('.product_title').should('contain', produto)
+    })
+
+    it('Deve visitar a página do produto', () => {
+        
+    })
+
+    it('Deve selecionar produto ao carrinho', () => {
+        
+    })
 })
